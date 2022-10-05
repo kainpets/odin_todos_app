880bd5687b92c841719a1d64dbd7deb62a7a80c9
kainpets <pawelstepniak22@gmail.com>
2022-10-05 22:21:41 +0200

    final commit

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,14 +1,17 @@
+import { Projects } from "./projects";
+
 export class UI {
   static renderTodos(todos) {
     const tableRow = document.querySelector(".insert-task");
 
     tableRow.innerHTML += `
     <tr>
+    <td>${todos.project-title}</td>
     <td>${todos.title}</td>
     <td>${todos.description}</td>
     <td>${todos.dueDate}</td>
     <td>${todos.priority}</td>
     </tr>
-    `
+    `;
   }
 }
